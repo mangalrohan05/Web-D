e3ea8ffb50cdc10ef2a14dfe68ce9300320fc035
Rohan Mangal <rohanmangal.2019@gmail.com>
2025-07-08 11:56:26 +0530

    do

---
+++ BeckEnd/V106 - React/my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <div style={{backgroundColor: 'blue', color: 'white', textAlign: 'center', height: '40px', alignContent: 'center'}}>This is a heading</div>
+    </>
+  )
+}
+
+export default App
